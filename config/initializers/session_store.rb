086989c32b39d3fc054de81949309bb5667bf3be
Ruturaj Choudhari <rpc4mv@virginia.edu>
2015-02-21 16:14:10 -0800

    change session_store key name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_rails-template_session'
+Rails.application.config.session_store :cookie_store, key: '_assemble-web_session'
