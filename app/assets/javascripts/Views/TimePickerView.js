1e7bf0fce4f9fb9a58a81f87d5abaa6fd74686e2
Ruturaj Choudhari <rpc4mv@virginia.edu>
2016-01-07 23:33:22 -0800

    WIP timeselect

--- app/assets/javascripts/Views/TimePickerView.js
+++ app/assets/javascripts/Views/TimePickerView.js
@@ -3,21 +3,15 @@ AssembleApp.Views.Timepicker = Backbone.View.extend({
 
     this.template = Handlebars.compile($('#time-picker-view-template').html());
 
-    function timeObjects(times) {
-      return _.map(times, function(i) { return { name: i, value: i}; });
-    }
+    var times = [];
+    for(var i=0; i<60; i++) { times.push({ name: i, value: i}); }
 
-    var times = [],
-        i;
-
-    for(i=0; i<13; i++) { times.push(i); }
     this.hourSelect = new AssembleApp.Views.CustomSelectView({
-      collection: new Backbone.Collection(timeObjects(times))
+      collection: new Backbone.Collection(times.slice(1, 13))
     });
 
-    for(i=13; i<31; i++) { times.push(i); }
     this.minuteSelect = new AssembleApp.Views.CustomSelectView({
-      collection: new Backbone.Collection(timeObjects(times))
+      collection: new Backbone.Collection(times)
     });
 
   },
@@ -26,7 +20,7 @@ AssembleApp.Views.Timepicker = Backbone.View.extend({
     var data = {};
 
     this.$el.html(this.template(data));
-    
+
     this.hourSelect.setElement(this.$('.hour')).render();
     this.minuteSelect.setElement(this.$('.minute')).render();
 
